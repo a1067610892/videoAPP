42f92e2d85574c56216f3c62996b4db3922d1773
“e <1067610892@qq.com>
2019-04-26 23:23:46 +0800

    添加了断网提示

--- src/Home/components/List.vue
+++ src/Home/components/List.vue
@@ -1,17 +1,24 @@
 <template>
-  <div class="list">
-    <div class="dome border_bottom" v-for="(item, index) in list" :key="index">
-      <img class="list-img" :src="item.images.large">
-      <div class="right">
-        <div class="list-left">
-          <h1 class="list-title">{{item.title}}</h1>
-          <h2>{{item.rating.average == 0 ? '暂无评分' : item.rating.average}}</h2>
-        </div>
-        <div class="list-right">
-          <span class="count">{{item.collect_count}}人看过</span>
-          <button class="border">购票</button>
+  <div>
+    <div class="list" v-if="onLine == true">
+      <div class="dome border_bottom" v-for="(item, index) in list" :key="index">
+        <img class="list-img" :src="item.images.large">
+        <div class="right">
+          <div class="list-left">
+            <h1 class="list-title">{{item.title}}</h1>
+            <h2 class="average">{{item.rating.average == 0 ? '暂无评分' : item.rating.average}}</h2>
+            <div class="director">导演：{{item.directors[0].name}}</div>
+            <div class="performer"><span>主演：</span><span v-for="(performer, index) in item.casts" :key="index">{{performer.name}}<span v-if="index + 1 != item.casts.length"> / </span></span></div>
+          </div>
+          <div class="list-right">
+            <span class="count">{{item.collect_count}}人看过</span>
+            <button class="border">购票</button>
+          </div>
         </div>
       </div>
+    </div>
+    <div class="onLine" v-if="onLine == false">
+      <p>网络无连接，请检查网络设置</p>
     </div>
   </div>
 </template>
@@ -21,30 +28,39 @@ export default {
   name: 'HomeList',
   data () {
     return {
-      list: []
+      list: [],
+      onLine: true
     }
   },
   mounted () {
-    this.getData()
+    this.offline()
   },
   methods: {
-    getData () {
-      this.axios.get(`/api/in_theaters?apikey=0b2bdeda43b5688921839c8ecb20399b&city=北京&start=0&count=39&client=&udid=""`).then((res) => {
-        console.log(res)
-        this.list = res.data.subjects
-        this.addZero(this.list)
-        this.count(this.list)
-      })
+    /* 判断是否有网络 */
+    offline () {
+      if (navigator.onLine === false) {
+        this.onLine = false
+      } else {
+        this.onLine = true
+        this.getData()
+      }
     },
-    /* APP */
     // getData () {
-    //   this.axios.get(`https://api.douban.com/v2/movie/in_theaters?apikey=0b2bdeda43b5688921839c8ecb20399b&city=北京&start=0&count=39&client=&udid=""`).then((res) => {
+    //   this.axios.get(`/api/in_theaters?apikey=0b2bdeda43b5688921839c8ecb20399b&city=北京&start=0&count=39&client=&udid=""`).then((res) => {
     //     console.log(res)
     //     this.list = res.data.subjects
     //     this.addZero(this.list)
     //     this.count(this.list)
     //   })
     // },
+    /* APP */
+    getData () {
+      this.axios.get(`https://api.douban.com/v2/movie/in_theaters?apikey=0b2bdeda43b5688921839c8ecb20399b&city=北京&start=0&count=39&client=&udid=""`).then((res) => {
+        this.list = res.data.subjects
+        this.addZero(this.list)
+        this.count(this.list)
+      })
+    },
     /* 评分人数简化 */
     count (arr) {
       for (let i = 0; i < arr.length; i++) {
@@ -96,6 +112,23 @@ export default {
     font-size: .426667rem;
     line-height: .533333rem;
   }
+  .average {
+    margin-top: .266667rem;
+  }
+  /* 导演 */
+  .director {
+    color: #999;
+    font-size: .266667rem;
+    margin-top: .133333rem;
+  }
+  /* 演员 */
+  .performer {
+    width: 90%;
+    color: #999;
+    margin-top: .133333rem;
+    font-size: .266667rem;
+    line-height: .4rem;
+  }
   .dome .right .list-right {
     width: 35%;
     float: right;
@@ -117,5 +150,20 @@ export default {
   .dome .right .list-right .border::before {
     border-color: #f48787;
     border-radius: .4rem;
+  }
+  .onLine {
+    width: 10rem;
+    position: absolute;
+    margin: 2.146667rem auto 0 auto;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+  }
+  .onLine p{
+    position: relative;
+    top: 45%;
+    font-size: .533333rem;
+    text-align: center;
   }
 </style>
