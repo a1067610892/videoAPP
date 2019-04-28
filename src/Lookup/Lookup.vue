f72b68c37978306d3b5b66b36daaf13cc9631675
“e <1067610892@qq.com>
2019-04-28 20:38:18 +0800

    完成找片页面的布局

--- src/Lookup/Lookup.vue
+++ src/Lookup/Lookup.vue
@@ -2,20 +2,42 @@
   <div class="lookup">
     <lookup-header></lookup-header>
     <common-nav :activeIndex="2"></common-nav>
+    <lookup-list :list="list"></lookup-list>
   </div>
 </template>
 
 <script>
 import CommonNav from '../common/Nav'
 import LookupHeader from './components/Header'
+import LookupList from './components/List'
 export default {
   name: 'Lookup',
   components: {
     CommonNav,
-    LookupHeader
+    LookupHeader,
+    LookupList
   },
   data () {
-    return {}
+    return {
+      list: []
+    }
+  },
+  mounted () {
+    this.getData()
+  },
+  methods: {
+    // getData () {
+    //   this.axios.get(`/api/in_theaters?apikey=0b2bdeda43b5688921839c8ecb20399b&city=北京&start=0&count=39&client=&udid=""`).then((res) => {
+    //     console.log(res)
+    //     this.list = res.data.subjects
+    //   })
+    // }
+    /* APP */
+    getData () {
+      this.axios.get(`https://api.douban.com/v2/movie/in_theaters?apikey=0b2bdeda43b5688921839c8ecb20399b&city=北京&start=0&count=39&client=&udid=""`).then((res) => {
+        this.list = res.data.subjects
+      })
+    }
   }
 }
 </script>
---
+++ src/Lookup/components/List.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="List">
+    <div class="dome" v-for="(item, index) in list" :key="index">
+      <img class="img" :src="item.images.large">
+      <h1 class="title">{{item.title}}</h1>
+      <span class="span" v-if="item.rating.average !== 0">{{(item.rating.average.toString()).length == 1 ? item.rating.average + '.0' : item.rating.average }}</span>
+      <span class="span" v-if="item.rating.average == 0">暂无评分</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Lookup',
+  props: {
+    list: Array
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+  .List {
+    width: 10rem;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+  .List .dome {
+    width: 2.8rem;
+    height: 5.066667rem;
+    text-align: center;
+    padding: .133333rem .266667rem;
+  }
+  .List .dome .img {
+    width: 100%;
+    height: 4rem;
+    border-radius: .2rem;
+  }
+  .title {
+    margin-top: .133333rem;
+    font-size: .32rem;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+  .span {
+    width: 100%;
+    height: .4rem;
+    line-height: .4rem;
+    display: inline-block;
+    margin-top: .133333rem;
+    font-size: .266667rem;
+  }
+</style>
